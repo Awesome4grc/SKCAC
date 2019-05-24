70c300ae33e8974623f7a59df5bd7e570a0d7ec9
Alec <avan-etten@mail.greenriver.edu>
2019-05-24 14:18:26 -0400

    increased button functionality to properly work with validation

--- JavaScript/nextButton.js
+++ JavaScript/nextButton.js
@@ -1,11 +1,40 @@
+/**
+ * Author: Awesome Four (Adolfo, Alec, Bo, Keith)
+ */
 
-$('.btn').on('click', function () {
-    var number = parseInt($(this).attr('id'));
-    var buttonId = 'personal' + (number + 1);
-    $('#' + buttonId).click();
+$('.btn-acc').on('click', function () {
+    if(validate($(this).attr('name'))) {
+        var number = parseInt($(this).attr('id'));
+        var buttonId = 'personal' + (number + 1);
+        $('#' + buttonId).click();
+    }
+
 });
 
 $('.btn-tab').on('click', function(){
     var tabName = $(this).attr('name');
     $(tabName).click();
 });
+
+function validate(name) {
+
+    var pass = true;
+
+// Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName(name);
+        console.log(forms);
+
+// Loop over them and prevent submission
+        Array.prototype.filter.call(forms, function (form) {
+                if (form.checkValidity() === false) {
+                    console.log("What");
+                    pass = false;
+                    console.log(form);
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            });
+    console.log(pass);
+    return pass;
+}
