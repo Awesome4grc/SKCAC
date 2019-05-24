8a883a87ab1247b54fe9672255cfc42565fdbc0f
Alec Van_Etten <avan-etten@mail.greenriver.edu>
2019-05-23 20:56:52 -0400

    added SKCACTest.php

---
+++ tdd/SKCACTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase as TestCase;
+
+class SKCACTest extends TestCase
+{
+    public function setUp()
+    {
+
+    }
+
+    public function tearDown()
+    {
+
+    }
+}
