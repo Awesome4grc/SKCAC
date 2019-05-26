3ba1db13d4ae88609296c957a6580e8cbcaedbcc
Alec <avan-etten@mail.greenriver.edu>
2019-05-25 21:20:06 -0400

    Created php script to put input variables into Session variables

---
+++ model/sessionBuilder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A-VE
+ * Date: 5/24/2019
+ * Time: 12:32 PM
+ */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$_SESSION[$_POST['id']] = $_POST['value'];
+
+//echo "Session ". $_SESSION[$_POST['id']];
