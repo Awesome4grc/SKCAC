64a2cbd96219548d7477639b299cbd7127ad7445
Alec <avan-etten@mail.greenriver.edu>
2019-05-25 21:19:34 -0400

    added AJAX request to put input values into Session variables

--- JavaScript/nextButton.js
+++ JavaScript/nextButton.js
@@ -22,9 +22,13 @@ function validate(name) {
 
 // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.getElementsByClassName(name);
+        var inputs = document.getElementsByTagName("input");
         console.log(forms);
 
 // Loop over them and prevent submission
+    for (let i = 0; i < inputs.length; i++) {
+            $.post('model/sessionBuilder.php', {value : inputs[i].value, id : inputs[i].id});
+    }
         Array.prototype.filter.call(forms, function (form) {
                 if (form.checkValidity() === false) {
                     console.log("What");
