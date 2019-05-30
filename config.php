0927141dad75c8c3c9632fc3eb91b0e12c17351b
Keith Carlson <avan-etten@mail.greenriver.edu>
2019-05-30 13:23:44 -0400

    recommiting changes

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define("DB_DSN", "mysql:dbname=awesomeg_grc");
-define("DB_USERNAME", "awesomeg_grcuser");
-define("DB_PASSWORD", "Bup@%t7h#QSN");
-
--- tdd/SKCACTest.php
+++ tdd/SKCACTest.php
@@ -1,16 +1,13 @@
+
 <?php
-
 use PHPUnit\Framework\TestCase as TestCase;
-
 class SKCACTest extends TestCase
 {
     private $participant;
-
     public function setUp()
     {
         $this->pariticipant = new Participant();
     }
-
     public function tearDown()
     {
         unset($this->participant);
